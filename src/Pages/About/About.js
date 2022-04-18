c4105c39a071c9c8c3f44c192ad6ccd97428cfb1
unknown <sayedpritom999@gmail.com>
2022-04-18 22:45:41 +0600

    about section added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
     return (
-        <div>
-            <h1>About me</h1>
+        <div className="about">
+            <div className="container">
+                <div className="about-box">
+                    <p>
+                        Hello there, <br />
+                        I'm Pritom. A junior web developer and still in the learning phase. I have a goal of becoming a full-stack developer in the next 2-to 3 years. I'm learning and practicing web technologies every day with that goal in mind.  The project you're looking at was done as an assignment project in the course of learning web development with Programming Hero.
+
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
